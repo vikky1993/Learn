0b3c654f17f57a8334bdb46b48f81a6e1e71cbde
vikky1993 <wickee0810@gmail.com>
2018-01-23 15:02:24 +0530

    add subtract multiply

---
+++ sdp.py
@@ -0,0 +1,9 @@
+""" WAP to find sum, difference, product of two numbers. """
+x =float(input("Enter a number >>>"))
+y =float(input("Enter another number >>>"))
+sum = x + y
+print("The sum of the two numbers entered is ", sum)
+difference = x - y
+print("The difference of the two numbers entered is ", difference)
+product = x * y
+print("The product of the two numbers entered is ", product)
